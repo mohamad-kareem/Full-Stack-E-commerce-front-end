213978ac6c2d639d11dac3edc10822b60f2e681c
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-05 15:03:52 +0200

    Adding the CheckTheCorrespondingTable function

--- javascript/admin.js
+++ javascript/admin.js
@@ -1,10 +1,12 @@
 const category_buttons = document.getElementsByClassName("Category-Buttons");
+const category_list = document.getElementsByClassName("List-block");
 
 for(let i=0; i< category_buttons.length; i++){
     category_buttons[i].addEventListener("click", () => {
-        let button = category_buttons[i];
+        let button = category_buttons[i];;
         TurnOffEveryButton();
         TurnOnTheSelectedButton(button);
+        CheckTheCorrespondingTable(button);
     });
 }
 
@@ -15,3 +17,9 @@ const TurnOffEveryButton = () => {
 }
 
 const TurnOnTheSelectedButton = (button) => button.classList.add("Button-enabled");
+
+const CheckTheCorrespondingTable = (button) => {
+    if (button.textContent == "List of Phones") {
+        category_list[1].classList.remove("List-disabled");
+    }
+}
